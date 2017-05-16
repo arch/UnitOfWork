2439f4e29dd55283a0d1573987b3834629668913
Sergey Ilyin <silyinnur@gmail.com>
2017-05-16 11:01:46 +0500

    Update Repository.cs

--- src/Microsoft.EntityFrameworkCore.UnitOfWork/Repository.cs
+++ src/Microsoft.EntityFrameworkCore.UnitOfWork/Repository.cs
@@ -106,9 +106,12 @@ namespace Microsoft.EntityFrameworkCore
         /// <param name="pageIndex">The index of page.</param>
         /// <param name="pageSize">The size of the page.</param>
         /// <param name="disableTracking"><c>True</c> to disable changing tracking; otherwise, <c>false</c>. Default to <c>true</c>.</param>
+        /// <param name="cancellationToken">
+        ///     A <see cref="CancellationToken" /> to observe while waiting for the task to complete.
+        /// </param>
         /// <returns>An <see cref="IPagedList{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
         /// <remarks>This method default no-tracking query.</remarks>
-        public async Task<IPagedList<TEntity>> GetPagedListAsync(Expression<Func<TEntity, bool>> predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, int pageIndex = 0, int pageSize = 20, bool disableTracking = true)
+        public async Task<IPagedList<TEntity>> GetPagedListAsync(Expression<Func<TEntity, bool>> predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, int pageIndex = 0, int pageSize = 20, bool disableTracking = true, CancellationToken cancellationToken = default(CancellationToken))
         {
             IQueryable<TEntity> query = _dbSet;
             if (disableTracking)
@@ -123,11 +126,11 @@ namespace Microsoft.EntityFrameworkCore
 
             if (orderBy != null)
             {
-                return await  orderBy(query).ToPagedListAsync(pageIndex, pageSize);
+                return orderBy(query).ToPagedListAsync(pageIndex, pageSize, 0, cancellationToken);
             }
             else
             {
-                return await query.ToPagedListAsync(pageIndex, pageSize);
+                return query.ToPagedListAsync(pageIndex, pageSize, 0, cancellationToken);
             }
         }
 
