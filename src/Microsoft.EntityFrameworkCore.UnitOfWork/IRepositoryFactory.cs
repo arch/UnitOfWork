fbb8747ab814532e362ac8cc76d84dc30b50eca9
Mohammad Mehrnia <qermezkon@gmail.com>
2019-03-17 00:55:31 +0330

    Add QueryRepository for T-SQL View (DbQuery)

--- src/Microsoft.EntityFrameworkCore.UnitOfWork/IRepositoryFactory.cs
+++ src/Microsoft.EntityFrameworkCore.UnitOfWork/IRepositoryFactory.cs
@@ -14,5 +14,12 @@ namespace Microsoft.EntityFrameworkCore
         /// <typeparam name="TEntity">The type of the entity.</typeparam>
         /// <returns>An instance of type inherited from <see cref="IRepository{TEntity}"/> interface.</returns>
         IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = false) where TEntity : class;
+        /// <summary>
+        /// Gets the specified query repository (DbQuery) for the <typeparamref name="TEntity"/>.
+        /// </summary>
+        /// <param name="hasCustomRepository"><c>True</c> if providing custom repositry</param>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <returns>An instance of type inherited from <see cref="IQueryRepository{TEntity}"/> interface.</returns>
+        IQueryRepository<TEntity> GetQueryRepository<TEntity>(bool hasCustomRepository = false) where TEntity : class;
     }
 }
--- src/Microsoft.EntityFrameworkCore.UnitOfWork/UnitOfWork.cs
+++ src/Microsoft.EntityFrameworkCore.UnitOfWork/UnitOfWork.cs
@@ -102,6 +102,38 @@ namespace Microsoft.EntityFrameworkCore
         }
 
         /// <summary>
+        /// Gets the specified query repository (DbQuery) for the <typeparamref name="TEntity"/>.
+        /// </summary>
+        /// <param name="hasCustomRepository"><c>True</c> if providing custom repositry</param>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <returns>An instance of type inherited from <see cref="IQueryRepository{TEntity}"/> interface.</returns>
+        public IQueryRepository<TEntity> GetQueryRepository<TEntity>(bool hasCustomRepository = false) where TEntity : class
+        {
+            if (repositories == null)
+            {
+                repositories = new Dictionary<Type, object>();
+            }
+
+            // what's the best way to support custom reposity?
+            if (hasCustomRepository)
+            {
+                var customRepo = _context.GetService<QueryRepository<TEntity>>();
+                if (customRepo != null)
+                {
+                    return customRepo;
+                }
+            }
+
+            var type = typeof(TEntity);
+            if (!repositories.ContainsKey(type))
+            {
+                repositories[type] = new Repository<TEntity>(_context);
+            }
+
+            return (IQueryRepository<TEntity>)repositories[type];
+        }
+
+        /// <summary>
         /// Executes the specified raw SQL command.
         /// </summary>
         /// <param name="sql">The raw SQL.</param>
