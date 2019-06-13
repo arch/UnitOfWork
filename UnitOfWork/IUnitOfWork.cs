92ec96e44ffb95f47c02377603daef271123ec21
Mohammad Mehrnia <qermezkon@gmail.com>
2019-06-13 13:06:25 +0430

    resolve bugs in IUnitOfWork

--- UnitOfWork/IUnitOfWork.cs
+++ UnitOfWork/IUnitOfWork.cs
@@ -31,7 +31,7 @@ namespace Arch.EntityFrameworkCore.UnitOfWork
         /// <param name="hasCustomRepository"><c>True</c> if providing custom repositry</param>
         /// <typeparam name="TEntity">The type of the entity.</typeparam>
         /// <returns>An instance of type inherited from <see cref="IRepository{TEntity}"/> interface.</returns>
-        IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = false) where TEntity : class;
+        IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = false, bool IsView = false) where TEntity : class;
 
         /// <summary>
         /// Saves all changes made in this context to the database.
--- UnitOfWork/UnitOfWork.cs
+++ UnitOfWork/UnitOfWork.cs
@@ -74,9 +74,10 @@ namespace Arch.EntityFrameworkCore.UnitOfWork
         /// Gets the specified repository for the <typeparamref name="TEntity"/>.
         /// </summary>
         /// <param name="hasCustomRepository"><c>True</c> if providing custom repositry</param>
+        /// <param name="IsView"><c>True</c> used dbQuery for t-sql views</param>
         /// <typeparam name="TEntity">The type of the entity.</typeparam>
         /// <returns>An instance of type inherited from <see cref="IRepository{TEntity}"/> interface.</returns>
-        public IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = false) where TEntity : class
+        public IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = false, bool IsView = false) where TEntity : class
         {
             if (repositories == null)
             {
